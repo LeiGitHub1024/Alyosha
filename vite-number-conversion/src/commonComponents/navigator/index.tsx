299791c4f7631c4249fec99830f155389e77291f
LeiGitHub1024 <cmcc.coder@gmail.com>
2023-01-18 12:05:08 +0800

    尝试修复移动端torch问题2

--- vite-number-conversion/src/commonComponents/navigator/index.tsx
+++ vite-number-conversion/src/commonComponents/navigator/index.tsx
@@ -5,28 +5,28 @@ import { IconMenuUnfold } from '@arco-design/web-react/icon';
 
 export const Navigator = (props:{MenuItems:any[]})=>{
   const {MenuItems} = props
-  const changeTabStatus = (target:any)=>{ //实现点击后改变样式
+  const changeTabStatus = (e:any)=>{ //实现点击后改变样式
     var menuItems = document.querySelectorAll('a');
     for(var item of menuItems){
       item.classList.remove('active');
     }
-    target.classList.add('active');
+    e.target.classList.add('active');
     
   }
  
   function onclickFun(){
     const navigatorCycle = document.querySelector('.navigatorCycle')
     // console.log(navigatorCycle?.classList.length)
-    navigatorCycle?.classList.length==2 ?     navigatorCycle?.classList.remove('navigatorCycle-hover') :     navigatorCycle?.classList.add('navigatorCycle-hover')
+    navigatorCycle?.classList.length==2? navigatorCycle?.classList.remove('navigatorCycle-hover') :     navigatorCycle?.classList.add('navigatorCycle-hover')
   }
 
   
   
 
   return(<div>
-    <div className="navigatorCycle" onTouchEnd={onclickFun}>
+    <div className="navigatorCycle" onTouchEnd={onclickFun}  onClick={onclickFun}>
       <div id="icon"><IconMenuUnfold /></div>
-      <div onClick={(e)=>changeTabStatus(e)} id='horizenMenu' className="menu">{...MenuItems}</div>
+      <div onTouchEnd={(e)=>changeTabStatus(e)} id='horizenMenu' className="menu">{...MenuItems}</div>
     </div>
     <div onClick={(e)=>changeTabStatus(e)} id='verticleMenu' className="menu">{...MenuItems}</div>
   </div>)
