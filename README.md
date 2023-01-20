e0e48bf5a6f027afa1afbfe8d0a20e40f0fbae8f
LeiGitHub1024 <cmcc.coder@gmail.com>
2023-01-20 08:20:57 +0800

    更新他山之石

--- README.md
+++ README.md
@@ -5,17 +5,18 @@
 
 
 ## 开发日志
-### 2023.1.11
-  build: 构建项目，使用npm vite react ts arco-design
-  fix: js和css404，解决方案：vite.config.ts 中加入   base:"/Alyosha/"
 
-### 2023.1.13
-  更改域名：alyosha.top, 删除 vite.config.ts 中加入的   base:"/Alyosha/"
-
-### 2023.1.17
-  更新了一些文章，改进了基本样式。
-
+### 2023.1.20
+  更新了他山之石板块，新增许多好东西。
 ### 2023.1.18
   “图片不压缩，前端要背锅”，新增tiny png，在需要的时候命令行 npx tiny ，之后按照提示将目标文件中中所有图像资源进行压缩。
   优化移动端样式，导航栏移动端适配。
   新增一键发布脚本 p.sh
+### 2023.1.17
+  更新了一些文章，改进了基本样式。
+### 2023.1.13
+  更改域名：alyosha.top, 删除 vite.config.ts 中加入的   base:"/Alyosha/"
+
+### 2023.1.11
+  build: 构建项目，使用npm vite react ts arco-design
+  fix: js和css404，解决方案：vite.config.ts 中加入   base:"/Alyosha/"
--- vite-number-conversion/src/pages/superman/index.tsx
+++ vite-number-conversion/src/pages/superman/index.tsx
@@ -7,21 +7,43 @@ const SuperMan = ()=>{
   // 个人技术播客
   const personBlogs = [
     <StyledLink icon={apng} shape='circle' title="阮一峰" desc="阮一峰的网络日志" link="https://www.ruanyifeng.com/blog/"></StyledLink>,
+    <StyledLink icon={apng} shape='circle' title="张洪Heo" desc="张洪Heo,设计师、产品经理、独立开发者、博主" link="https://blog.zhheo.com/about/"></StyledLink>,
+    <StyledLink icon={apng} shape='circle' title="山月" desc="前端工程师" link=" https://q.shanyue.tech/"></StyledLink>,
   ]
 
   const magicSites = [    
     <StyledLink  title="ChatGPT" desc="最好用的自然语言生成模型" link="https://chat.openai.com/chat"></StyledLink>,
     <StyledLink  title="Animated Drawings" desc="让2D简笔画动起来" link="https://sketch.metademolab.com/canvas"></StyledLink>,
-  
-
-  ]
+    <StyledLink  title="TOONME" desc="图像卡通化" link="https://toonme.com/result"></StyledLink>,
+    <StyledLink  title="MatrixDigitalRain" desc="黑客帝国代码雨" link="https://codepen.io/yuanchuan/pen/YoqWeR"></StyledLink>,
+    <StyledLink  title="AI绘画资源合集" desc="包含国内外可使用平台、使用教程、参数教程、部署教程、业界新闻等等" link="  https://github.com/hua1995116/awesome-ai-painting"></StyledLink>,
+    <StyledLink  title="Paper2GUI" desc="让每个人都简单方便的使用前沿人工智能技术" link="https://github.com/Baiyuetribe/paper2gui"></StyledLink>,
+    <StyledLink  title="彩云小梦" desc="AI讲故事" link="https://if.caiyunai.com/dashboard/novels"></StyledLink>,
+     ]
 
 const utils = [
   <StyledLink   title="Convertio" desc="文件格式转换" link="https://convertio.co/zh/"></StyledLink>,
+  <StyledLink  title="视频转换GIF" desc="视频转换成GIF" link="https://airmore.cn/video-to-gif-online"></StyledLink>,
+  <StyledLink  title="ColorAvatar" desc="卡通头像生成" link="  https://vue-color-avatar.vercel.app/"></StyledLink>,
+  <StyledLink  title="色图网站" desc="字面意思" link="https://colorsite.librian.net/"></StyledLink>,
+  <StyledLink  title="ParticleButtons" desc="React粒子效果按钮" link="https://transitive-bullshit.github.io/react-particle-effect-button/"></StyledLink>,
+  <StyledLink  title="Favioli" desc="Emoji Favicons for the web" link="https://favioli.com/"></StyledLink>,
+  <StyledLink  title="渐变颜色" desc="生成渐变色css代码" link="http://color.oulu.me/"></StyledLink>,
+  <StyledLink  title="BoardMix" desc="在线绘制好看的流程图" link="https://boardmix.cn/app/my-space"></StyledLink>,
+  <StyledLink  title="PageSpeedInsights" desc="网站性能评测" link="https://pagespeed.web.dev/"></StyledLink>,
+
 ]
 const study = [
   <StyledLink  title="从零开始的神经网络" desc="深度学习入门" link="https://aegeorge42.github.io/"></StyledLink>,
   <StyledLink  title="GithBranching" desc="Github入门" link="https://learngitbranching.js.org/?locale=zh_CN"></StyledLink>,
+  
+]
+
+const games = [
+  <StyledLink  title="Mikutap" desc="轻音游戏" link="https://aidn.jp/mikutap/"></StyledLink>,
+  <StyledLink  title="LOLDodgeGame." desc="英雄联盟走位小游戏" link="https://loldodgegame.com/play/"></StyledLink>,
+  <StyledLink  title="VENGE" desc="FPS游戏" link="https://venge.io/?create-account"></StyledLink>,
+  <StyledLink  title="ChornoDivide" desc="网页版红警2" link="https://chronodivide.com/"></StyledLink>,
 
 ]
 
@@ -29,9 +51,8 @@ const study = [
     <CommonCard title='个人技术博客' links={personBlogs} ></CommonCard>
     <CommonCard title='奇妙网站' links={magicSites} ></CommonCard>
     <CommonCard title='我爱学习' links={study} ></CommonCard>
+    <CommonCard title='Web游戏' links={games} ></CommonCard>
     <CommonCard title='实用工具' links={utils} ></CommonCard>
-
-
   </div>)
 }
 export default SuperMan
