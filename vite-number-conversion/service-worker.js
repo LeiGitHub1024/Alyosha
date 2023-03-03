5aca12d3ec71640e69ba787bb56c54a12c68a647
mengxiangyu.taro <mengxiangyu.taro@bytedance.com>
2023-03-03 18:14:57 +0800

    新增pwa

---
+++ vite-number-conversion/service-worker.js
@@ -0,0 +1,28 @@
+
+self.addEventListener("install", (event) => {
+  console.log("service worker: install", event);
+});
+
+self.addEventListener('activate', async event => {
+  console.log("service worker: activate", event);
+});
+
+self.addEventListener("fetch", (event) => {
+  console.debug("network fetch:", event);
+});
+
+self.addEventListener("push", e => {
+  // const { title, text, url } = e.data.json();
+  const title = "Service Worker";
+  const text = e.data.text();
+  const options = {
+    title: title,
+    body: text,
+    // data: url,
+  };
+  e.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener("notificationclick", e => {
+  console.log("service worker: notificationclick", e.notification);
+});
